acc667d78e8096b1ca6b7d0e26f6a19d63615d93
Tanula98 <tanula98zaa@gmail.com>
2020-09-24 18:06:16 +0300

    Scripted auto-commit on change (2020-09-24 18:06:16) by gitwatch.sh

--- src/main/java/Servers.java
+++ src/main/java/Servers.java
@@ -2,6 +2,15 @@
 
 
 public class Servers {
+    private List<String> serversList;
+
+    public Servers(List<String> serversList) {
+        this.serversList = serversList;
+    }
+
+    public List<String> getServersList() {
+        return serversList;
+    }
 
 
 }
