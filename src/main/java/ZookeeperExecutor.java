3ced31cae2e0c56542943d448403c74700da8bff
Tanula98 <tanula98zaa@gmail.com>
2020-09-29 12:53:31 +0300

    Scripted auto-commit on change (2020-09-29 12:53:31) by gitwatch.sh

--- src/main/java/ZookeeperExecutor.java
+++ src/main/java/ZookeeperExecutor.java
@@ -10,7 +10,7 @@ public class ZookeeperExecutor implements Watcher {
     private ZooKeeper zoo;
 
     ZookeeperExecutor(ActorRef cacheActor, int serverPort) throws IOException, KeeperException, InterruptedException {
-        zoo = new
+        zoo = new ZooKeeper();
 
 
     }
