486a0db2c8c430ea7141b2143a73af97548e4222
Tanula98 <tanula98zaa@gmail.com>
2020-09-29 12:51:22 +0300

    Scripted auto-commit on change (2020-09-29 12:51:22) by gitwatch.sh

--- src/main/java/ZookeeperExecutor.java
+++ src/main/java/ZookeeperExecutor.java
@@ -1,6 +1,11 @@
+import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
 public class ZookeeperExecutor implements Watcher {
 
 
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
